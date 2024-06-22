4a2a84d510cff7bbc953c689d1f94f6060e0903c
Reem Hassan <reemhassan742r@gmail.com>
2024-06-22 19:19:41 +0300

    Update 242. Valid Anagram.py

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -1,1 +1,9 @@
-print("gg")
+class Solution:
+    def isAnagram(self, s, t):
+        return sorted(s) == sorted(t)
+
+if __name__ == "__main__":
+    s = "anagram"
+    t = "nagaram"
+    solution = Solution()
+    print(solution.isAnagram(s, t))  
